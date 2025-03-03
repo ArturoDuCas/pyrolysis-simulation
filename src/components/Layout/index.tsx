98205169c7e9fcbc7854384c5b12626e30ea7dfd
arturodcs <a00833516@tec.mx>
2025-03-03 15:10:30 -0600

    chore: update palette

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
-import { Box } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
 interface Props {
   children: ReactNode;
@@ -8,14 +9,16 @@ interface Props {
 const Layout: FC<Props> = ({ children }) => {
 
   return (
-    <Box sx={{
-      width: "100vw",
-      height: "100vh",
-      display: "flex",
-      flexDirection: "column",
-    }}>
-      {children}
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}>
+        {children}
+      </Box>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/Layout/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#cf302b', 
+    },
+    secondary: {
+      main: '#701e19', 
+    },
+  },
+});
